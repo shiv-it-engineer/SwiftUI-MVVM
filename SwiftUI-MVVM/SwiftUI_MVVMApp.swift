7ab005e508d27767be826919ee01f31f9e0597f4
Shiv Kumar <shivkumar@airasia.com>
2023-05-28 10:32:15 +0530

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Shiv kumar on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
